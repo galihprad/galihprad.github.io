2f8a61a8cdb31f924eb79e759e2f0664dfb52131
galihprad <galih@mail.unnes.ac.id>
2024-09-21 09:54:06 +0700

    Add home page

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,6 +13,9 @@ module.exports = {
    * Gatsby has a rich ecosystem of plugins.
    * If you need any more you can search here: https://www.gatsbyjs.com/plugins/
    */
+  siteMetadata: {
+    title: "Galih Pradipto Wisnujati"
+  },
   plugins: [
     {
       /**
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,84 +1,29 @@
 import React from "react"
-import { Link, graphql } from "gatsby"
+import { graphql } from "gatsby"
 
-import Bio from "../components/bio"
 import Layout from "../components/layout"
-import SEO from "../components/seo"
+import Seo from "../components/seo"
+import Bio from "../components/bio"
 
-const BlogIndex = ({ data, location }) => {
-    const siteTitle = data.site.siteMetadata?.title || `Title`
-    const posts = data.allMarkdownRemark.nodes
-
-    if (posts.length === 0) {
-        return (
-            <Layout location={location} title={siteTitle}>
-                <SEO title="Home" />
-                <Bio homepage />
-                <p>
-                    No blog posts found. Add markdown posts to "content/blog" (or the
-                    directory you specified for the "gatsby-source-filesystem" plugin in
-                    gatsby-config.js).
-                </p>
-            </Layout>
-        )
-    }
+const HomePage = ({ data, location }) => {
+    const siteTitle = data.site.siteMetadata.title
 
     return (
         <Layout location={location} title={siteTitle}>
-            <SEO title="Home" />
+            <Seo title="Home Page" />
             <Bio homepage />
-            {posts.map(post => {
-                const title = post.frontmatter.title || post.fields.slug
-                return (
-                    <article
-                        key={post.fields.slug}
-                        className="post-list-item"
-                        itemScope
-                        itemType="http://schema.org/Article"
-                    >
-                        <header>
-                            <h2>
-                                <Link to={post.fields.slug} itemProp="url">
-                                    <span itemProp="headline">{title}</span>
-                                </Link>
-                            </h2>
-                            <small>{post.frontmatter.date}</small>
-                        </header>
-                        <section>
-                            <p
-                                dangerouslySetInnerHTML={{
-                                    __html: post.frontmatter.description || post.excerpt,
-                                }}
-                                itemProp="description"
-                            />
-                        </section>
-                    </article>
-                )
-            })}
+            I am Galih Pradipto Wisnujati
         </Layout>
     )
 }
 
-export default BlogIndex
+export default HomePage
 
 export const pageQuery = graphql`
   query {
     site {
       siteMetadata {
         title
-      }
-    }
-    allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
-      nodes {
-        excerpt
-        fields {
-          slug
-        }
-        frontmatter {
-          date(formatString: "MMMM DD, YYYY")
-          title
-          description
-        }
       }
     }
   }
